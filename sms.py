5c18ad5320eb5e180a9e7f774e0e8b841d99e529
kamelzadeh <77294983+kamelzadeh@users.noreply.github.com>
2021-01-14 00:28:27 +0330

    Add files via upload

--- sms.py
+++ sms.py
@@ -1,12 +1,13 @@
 from kavenegar import *
 import pandas as pd
-cells= pd.read_excel ('sms.xlsx')
+cells = pd.read_excel('sms.xlsx', converters={'number': '{:0>11}'.format})
 sms_number=(cells['number'])
 payamak=(cells['message'])
 customer=(cells['name'])
+print(cells.columns)
 for i in (payamak.index):
     api = KavenegarAPI('7666717061636236495746553246436532654E6B364B46647238374B7679466F4175534D2F7476663737733D')
     params = {    'sender': '1000596446',    'receptor': sms_number[i],    'message': " سلام "+customer[i]+" خوبی "}
-#    print(sms_number[i], payamak[i])
+    print(sms_number[i], payamak[i])
     response = api.sms_send(params)
 
